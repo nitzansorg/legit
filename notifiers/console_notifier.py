10bc7f04ad96c9803a30d7b8f1aee83128d14e58
nitzan <a@gmail.com>
2024-03-29 22:37:41 +0300

    formatting2

--- notifiers/console_notifier.py
+++ notifiers/console_notifier.py
@@ -6,6 +6,6 @@ class ConsoleNotifier(INotifier):
     notifies to the console, in a readable human formatting
     """
     def notify(self, notification: Notification):
-        print(f"suspicious behavior was found.\n"
+        print(f"suspicious behavior was found!\n"
               f"suspicious event type: {notification.event_type}\n"
               f"suspect reason: {notification.suspect_reason}")
