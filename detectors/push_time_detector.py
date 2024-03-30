4ad1fb64c3afbcbccf35cf4a5d093da7f99954be
nitzan <a@gmail.com>
2024-03-30 10:51:31 +0300

    refered to utc

--- detectors/push_time_detector.py
+++ detectors/push_time_detector.py
@@ -9,11 +9,15 @@ BASIC_TIME_FORMAT = "%H:%M:%S"
 
 class PushTimeDetector(IDetector):
     """
-    detects a suspicious repo update by the time range it happened at
+    detects a suspicious push by the time range it happened at
     """
     def __init__(self,
                  suspicious_start_time: str = "6:00:00",
                  suspicious_end_time: str = "23:00:00"):
+        """
+        :param suspicious_start_time: the start of the suspicious time range in UTC
+        :param suspicious_end_time: the end of the suspicious time range in UTC
+        """
         self._suspicious_start_time: datetime = datetime.strptime(suspicious_start_time, BASIC_TIME_FORMAT)
         self._suspicious_end_time: datetime = datetime.strptime(suspicious_end_time, BASIC_TIME_FORMAT)
 
--- github_time_utils.py
+++ github_time_utils.py
@@ -3,6 +3,6 @@ from datetime import datetime
 
 def convert_github_time(time) -> datetime:
     if isinstance(time, str):
-        return datetime.fromisoformat(time)
+        return datetime.fromisoformat(time[:-1])  # covert after removing the 'Z' representing UTC
     else:  # assume the time is a timestamp
         return datetime.utcfromtimestamp(time)
